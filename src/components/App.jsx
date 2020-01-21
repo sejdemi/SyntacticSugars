73e272061420a1fb1355ada5d44cfddffc4ceb3e
Sejan Miah <sejan.miah@gmail.com>
2020-01-21 11:55:47 -0500

    Add in styled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,24 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import DummyComponent from './DummyComponents.js';
+import styled from 'styled-components';
 
 //create dummy components here and render them onto the screen
 //once they are rendered then begin the process of finalizing their layout
 //upon pulling requests from rest of team replace dummy components with actual components
 //test in browser to see how dimensions change from a desktop to mobile version
 
+const AppLayout = styled.div`
+padding: 40px;
+display: grid;
+grid-template-columns: 180px auto 100px 100px`;
 
 
 class App extends Component {
   // renders a button and a boolean; the button flips the boolean
   render() {
     return (
+      <AppLayout>
       <div>
         <h1>App</h1>
         <div style={{width: '100%', backgroundColor:'blue'}}>
@@ -33,6 +39,7 @@ class App extends Component {
           <DummyComponent/>
         </div>
       </div>
+      </AppLayout>
     );
   }
 }
