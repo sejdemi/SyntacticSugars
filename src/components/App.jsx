755851e714a472b1412c14203efc45560cba1374
Sejan Miah <sejan.miah@gmail.com>
2020-01-21 11:18:20 -0500

    Render Dummy Components onto page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,39 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import DummyComponent from './DummyComponents.js';
+
+//create dummy components here and render them onto the screen
+//once they are rendered then begin the process of finalizing their layout
+//upon pulling requests from rest of team replace dummy components with actual components
+//test in browser to see how dimensions change from a desktop to mobile version
+
+
 
 class App extends Component {
   // renders a button and a boolean; the button flips the boolean
   render() {
-    return (<h1>App</h1>);
+    return (
+      <div>
+        <h1>App</h1>
+        <div style={{width: '100%', backgroundColor:'blue'}}>
+          <h1>DUMMY COMPONENT ONE</h1>
+          <DummyComponent/>
+        </div>
+        <div style={{width: '100%', backgroundColor:'red'}}>
+        <h1>DUMMY COMPONENT TWO</h1>
+          <DummyComponent/>
+        </div>
+        <div style={{width: '100%', backgroundColor:'yellow'}}>
+        <h1>DUMMY COMPONENT THREE</h1>
+          <DummyComponent/>
+        </div>
+        <div style={{width: '100%', backgroundColor:'purple'}}>
+        <h1>DUMMY COMPONENT FOUR</h1>
+          <DummyComponent/>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/components/DummyComponents.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DummyComponent extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <h1>DUMMY COMPONENT NUMBER ONE</h1> */}
+            </div>
+        )
+    }
+}
+
+export default DummyComponent;
